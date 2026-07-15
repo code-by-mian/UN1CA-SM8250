9061598154668c9922040aa7e26ff7bfd88f135f
AstroByteX-Code <codebymianhamza@gmail.com.com>
2026-07-15 23:06:25 +0500

    modified

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -12,14 +12,3 @@ else
     echo "Unknown boot animation resolution for \"$TARGET_CODENAME\""
 fi
 
-LOG_STEP_IN "- Adding S25 Sounds"
-DELETE_FROM_WORK_DIR "system" "system/media/audio"
-ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio"
-SET_PROP "vendor" "ro.config.ringtone" "ACH_Galaxy_Bells.ogg"
-SET_PROP "vendor" "ro.config.notification_sound" "ACH_Brightline.ogg"
-SET_PROP "vendor" "ro.config.alarm_alert" "ACH_Morning_Xylophone.ogg"
-SET_PROP "vendor" "ro.config.media_sound" "Media_preview_Touch_the_light.ogg"
-SET_PROP "vendor" "ro.config.ringtone_2" "ACH_Atomic_Bell.ogg"
-SET_PROP "vendor" "ro.config.notification_sound_2" "ACH_Three_Star.ogg" 
-LOG_STEP_OUT
-
